a5927194605aa221ff4c2831531176f11ff15d14
limmi26 <kisamakiazuel@gmail.com>
2021-07-25 00:00:19 +0700

    perubahan 1

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // Kerjakan kode kalian di bawah ini
+
+var nama = "Michelle Iskak"; // Nama Siswa
+var nilai = "0"; // Nilai Siswa
+
+if ((nilai >= 101)) {
+console.log("Nilai Invalid") // jika nilai lebih dari 100 maka hasil eror
+}
+else if ((nilai >= 82)  && (nilai <= 100)) {
+console.log("Skor " + [nama] + " : A");
+}
+else if ((nilai >= 68 ) && (nilai <= 81)) {
+console.log("Skor " + [nama] + " : B");
+}
+else if ((nilai >= 50 ) && (nilai <= 67)) {
+console.log("Skor " + [nama] + " : C");
+}
+else if ((nilai >= 31 ) && (Nilai <= 49)) {
+console.log("Skor " + [nama] + " : D");
+}
+else if ((nilai >= 0 ) && (nilai <= 30)) {
+console.log("Skor " + [nama] + " : E");
+} else {
+console.log("Nilai Invalid"); // jika nilai kurang dari 0 maka hasil eror
+} 
