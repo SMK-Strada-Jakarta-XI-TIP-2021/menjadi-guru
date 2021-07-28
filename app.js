f7882451696bc4dd3c8c0b6104447d14a26a2b22
gabriellenatt <gabriellenatasha525@gmail.com>
2021-07-28 12:06:19 +0700

    Tugas menjadi guru

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // Kerjakan kode kalian di bawah ini
+
+var nama = 'gabrielle';
+var nilai = 100;
+
+if (nilai < 0 || niali > 100) {
+    console.log(nama+ ' ' + "nialinya ngaco nih, coba di periksa lagi!");
+} else if (niali >= 0 && niali <= 30) {
+    console.log(nama+ ' ' + "Kamu dapat E nih, ayo tingkatkan lagi!");
+} else if (niali >= 31 && nilai <= 49) {
+    console.log(nama+ ' ' + "Kamu dapet D nih, ayo tingkatkan lagi!");
+} else if (niali >= 50 && nilai <= 67) {
+    console.log(nama+ ' ' + "Kamu dapat C, lumayan nih!");
+} else if (niali >= 68 && 81) {
+    console.log(nama+ ' ' + "Kamu dapet B, Bagus nih udah di atas kkm!");
+} else if (niali >= 82 && nilai <=100) {
+    console.log(nama+ ' ' + "Cieee dapet 100 nih, bagi jawabannya doong");
+}
